2c14a9a64615261e86d6b69ede7ef1e058abe204
CDX <80155611+CDX-1@users.noreply.github.com>
2024-09-02 12:57:05 -0400

    Added a PluginInstallation Exception

--- api/src/main/java/net/minesprawl/api/exceptions/PluginInstallationException.java
+++ api/src/main/java/net/minesprawl/api/exceptions/PluginInstallationException.java
@@ -1,6 +1,6 @@
 package net.minesprawl.api.exceptions;
 
-public class PluginInstallationException extends Exception {
+public class PluginInstallationException extends RuntimeException {
     public PluginInstallationException(String message) {
         super(message);
     }
