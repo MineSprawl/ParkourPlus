c493a4eb0f838953a173a9d51b24dbbbd61c9165
CDX <80155611+CDX-1@users.noreply.github.com>
2024-09-02 16:47:03 -0400

    Main class

--- plugin/src/main/java/net/minesprawl/parkourPlus/ParkourPlus.java
+++ plugin/src/main/java/net/minesprawl/parkourPlus/ParkourPlus.java
@@ -1,16 +1,25 @@
 package net.minesprawl.parkourPlus;
 
+import net.minesprawl.api.ParkourManager;
+import net.minesprawl.api.ParkourManagerProvider;
+import net.minesprawl.parkourPlus.impl.ParkourManagerImpl;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ParkourPlus extends JavaPlugin {
+public final class ParkourPlus extends JavaPlugin implements ParkourManagerProvider {
+    private ParkourManager parkourManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        parkourManager = new ParkourManagerImpl(this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    @Override
+    public ParkourManager getManager() {
+        return parkourManager;
     }
 }
