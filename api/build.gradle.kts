9e42ac541da56fbdae7648235990b81f8034f84e
CDX <80155611+CDX-1@users.noreply.github.com>
2024-09-02 21:09:28 -0400

    Updated build scripts, 1.0.1

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    `maven-publish`
 }
 
 group = "net.minesprawl"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -13,8 +14,25 @@ repositories {
     }
 }
 
+
+
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "net.minesprawl"
+            artifactId = "parkourplus-api"
+            version = "1.0.1"
+        }
+    }
+}
+
+tasks.named("publishToMavenLocal") {
+    dependsOn(tasks.named("assemble"))
 }
 
 val targetJavaVersion = JavaVersion.VERSION_17
